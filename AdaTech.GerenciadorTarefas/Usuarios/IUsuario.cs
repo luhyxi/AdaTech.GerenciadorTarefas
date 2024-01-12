99f78a6f9bbb5934ed161a6a042160a5053cbe52
Luana <luhyxi@gmail.com>
2024-01-11 22:32:35 -0300

    feat: Added an interface for users

---
+++ AdaTech.GerenciadorTarefas/Usuarios/IUsuario.cs
@@ -0,0 +1,10 @@
+﻿using AdaTech.GerenciadorTarefas.Tarefas;
+
+namespace AdaTech.GerenciadorTarefas.Usuarios
+{
+    internal interface IUsuario
+    {
+        void AssumirTarefa();
+        void CriarTarefa(); 
+    }
+}
